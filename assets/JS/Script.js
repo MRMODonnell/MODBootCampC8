9d763c02482df4a8ca1c7f4c44d052c00c86ed54
Michael O'Donnell <michael.odonnell@st-annes.org.uk>
2023-01-30 10:39:02 +0000

    Struggled with challange so wiped previous code and starting over

--- assets/JS/Script.js
+++ assets/JS/Script.js
@@ -1,8 +1,8 @@
-localStorage.clear();
+
 
 let apiKey="2af9e902c491e50f4d7a7dbaa52ae4c3"
 
-let cityName="london"
+// let cityName="london"
 
 
 // let apiURL= "https://api.openweathermap.org/data/2.5/weather?q="+ cityName + "&units=imperial&appid=" +apiKey
@@ -10,17 +10,26 @@ let cityName="london"
 // console.log(apiURL)
 
 function findCityName(){
-    // let cityName = $("#search-input");
+    let cityName = titleCase($("#cityName")[0].value.trim());
 
    let apiURL= "https://api.openweathermap.org/data/2.5/weather?q="+ cityName + "&units=imperial&appid=" +apiKey
    
-   fetch(apiURL).then(function (response) {
+  let weatherdata= fetch(apiURL).then(function (response) {
         if (response.ok) {
             response.json().then(function (data) {
 
-               console.log(apiURL)
+                $("#cityName"-input)[0].textContent = cityName + " (" + moment().format('M/D/YYYY') + ")";
 
-                
+                $("#city-list").append('<button type="button" class="list-group-item list-group-item-light list-group-item-action city-name">' + cityName);
+console.log(apiURL)
+               let lattitude = data.coord.lat;
+               let longittude = data.coord.lon;
+
+               let Cordinates = lattitude.tostring() + " " + longittude.tostring();
+
+               console.log(Cordinates)
+
+              
             })
         } else {
             alert("I'm Sorry - you might have mispelt the city name wrong - the system has been unable to find the city requested - please try again");
@@ -35,3 +44,4 @@ $("#search-button").on("click", function () {
 
     $("form")[0].reset();
 })
+
